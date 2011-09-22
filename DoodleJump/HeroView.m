7a0692d9f5c518d0577a078e8ee3e7c3c9defd13
Dmitry Ivanov <id.ajantis@gmail.com>
2011-09-22 19:39:19 +0400

    [add] player animation

--- DoodleJump/HeroView.m
+++ DoodleJump/HeroView.m
@@ -23,7 +23,33 @@
         
         self.position = hero.position;
         
-        player = [CCSprite spriteWithFile:@"player.png"];
+        CCSpriteBatchNode *heroAnimationSheet = [CCSpriteBatchNode spriteSheetWithFile:@"player_animations.png"];
+        [self addChild:heroAnimationSheet];
+        
+        //player = [CCSprite spriteWithFile:@"player.png"];
+                
+        player = [CCSprite spriteWithTexture:heroAnimationSheet.texture rect:CGRectMake(0, 0, 255, 267)];
+        
+        CCAnimation *heroAnimation = [CCAnimation animationWithName:@"heroAnimation" delay:0.1f];
+        
+        int frameCount = 0;
+        for (int y = 0; y < 5; y++) {
+            for (int x = 0; x < 5; x++) {
+                CCSpriteFrame *frame = [CCSpriteFrame frameWithTexture:heroAnimationSheet.texture rect:CGRectMake(x*255,y*267,255,267)];
+
+                [heroAnimation addFrame:frame];
+                
+                frameCount++;
+                
+                if (frameCount == 24)
+                    break;
+            }
+        }
+        
+        CCAnimate *heroAction = [CCAnimate actionWithAnimation:heroAnimation];
+        CCRepeatForever *repeat = [CCRepeatForever actionWithAction:heroAction];
+        [player runAction:repeat];
+        
         player.scale = 0.16;
         player.anchorPoint = ccp(0.5, 0.0);
         player.position = ccp(0,-2);
